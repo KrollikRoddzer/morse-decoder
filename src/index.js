25992617884f33563a97e6e77ec337411fc12241
KrollikRoddzer <krollikroddzer@mail.ru>
2023-08-13 13:44:16 +0300

    feat: sovled the problem

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let ans = '';
+    for (let i = 0; i < expr.length; i += 10)
+    {
+        arr.push(expr.substr(i, 10));
+    }
+    arr.forEach(str => {
+        let code = '';
+        for (let i = 0; i < str.length; i += 2)
+        {
+            if (str[i] == '1' && str[i + 1] == '0')
+            {
+                code += '.';
+            }
+            if (str[i] == '1' && str[i + 1] == '1')
+            {
+                code += '-';
+            }
+            if (str[i] == '*')
+            {
+                code = '*';
+                break;
+            }
+        }
+        if (code == '*')
+        {
+            ans += ' ';
+        }
+        else
+        {
+            ans += MORSE_TABLE[code];
+        }
+    });
+    
+    return ans;
 }
 
 module.exports = {
